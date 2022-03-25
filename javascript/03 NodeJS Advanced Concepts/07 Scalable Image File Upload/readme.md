a3fbd7227eb823c68f7b4537a27ef7c9d3ad01cd
Isaac Paz <isaac.paz@unosquare.com>
2022-03-25 09:40:47 -0400

    03/Course code completed

---
+++ javascript/03 NodeJS Advanced Concepts/07 Scalable Image File Upload/readme.md
@@ -0,0 +1,7 @@
+# Scalable Image File Upload
+
+The best approach to make a Scalable File upload feature on our applications would be using external storage service like amazon, azure or google solutions out there.
+
+this because it is not expensive, it is scalable, automated backup and it is centralized
+
+###
---
+++ javascript/03 NodeJS Advanced Concepts/app/routes/uploadRoutes.js
@@ -0,0 +1,28 @@
+const AWS = require("aws-sdk");
+const uuid = require("uuid/v1");
+const keys = require("../config/keys");
+const requireLogin = require("../middlewares/requireLogin");
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey,
+  },
+  region: "us-east-1",
+});
+
+module.exports = (app) => {
+  app.get("/api/upload", requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl(
+      "putObject",
+      {
+        Bucket: "my-blog-ispm",
+        ContentType: "image/jpeg",
+        Key: key,
+      },
+      (err, url) => res.send({ key, url })
+    );
+  });
+};
