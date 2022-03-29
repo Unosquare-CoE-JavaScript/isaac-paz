35018a26a8278482e96647e5bc57809311fc3fc5
Isaac Paz <isaac.paz@unosquare.com>
2022-03-29 17:02:22 -0400

    04/ chapter 6 completed

---
+++ javascript/04 Multithreaded Javascript/6.- Multithreaded Patterns (194)/ch6-actors/actor.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const net = require("net");
+const RpcWorkerPool = require("./rpc-worker.js");
+
+const [, , host] = process.argv;
+const [hostname, port] = host.split(":");
+const worker = new RpcWorkerPool("./worker.js", 4, "leastbusy");
+
+const upstream = net
+  .connect(port, hostname, () => {
+    console.log("connected to server");
+  })
+  .on("data", async (raw_data) => {
+    const chunks = String(raw_data).split("\0");
+    chunks.pop();
+    for (let chunk of chunks) {
+      const data = JSON.parse(chunk);
+      const value = await worker.exec(data.method, ...data.args);
+      upstream.write(
+        JSON.stringify({
+          id: data.id,
+          value,
+          pid: process.pid,
+        }) + "\0"
+      );
+    }
+  })
+  .on("end", () => {
+    console.log("disconnect from server");
+  });
---
+++ javascript/04 Multithreaded Javascript/6.- Multithreaded Patterns (194)/ch6-mutex/thread-product-multex.js
@@ -0,0 +1,22 @@
+const { Worker, isMainThread, workerData } = require("worker_threads");
+const Mutex = require("./mutex");
+const assert = require("assert");
+if (isMainThread) {
+  const shared = new SharedArrayBuffer(4 * 4 * 4);
+  const sharedInts = new Int32Array(shared);
+  sharedInts.set([2, 3, 5, 7]);
+  for (let i = 0; i < 3; i++) {
+    new Worker(__filename, { workerData: { i, shared } });
+  }
+} else {
+  const { i, shared } = workerData;
+  const sharedInts = new Int32Array(shared);
+  const mutex = new Mutex(sharedInts, 4);
+  mutex.exec(() => {
+    const a = sharedInts[i];
+    for (let j = 0; j < 1_000_000; j++) {}
+    const b = sharedInts[3];
+    sharedInts[3] = a * b;
+    assert.strictEqual(sharedInts[3], a * b);
+  });
+}
